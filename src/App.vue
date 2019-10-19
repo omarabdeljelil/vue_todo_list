0a830aa1515ec1b35af9f4b0ebe5f3c99cda6d81
Omar Abdeljelil <omarabdeljelil@gmail.com>
2019-10-18 22:53:56 -0400

    Todos: crud without persisting change

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Header/>
+        <AddTodo v-on:add-todo ="addTodo"/>
+        <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Header from './components/layout/Header';
+    import Todos from './components/Todos';
+    import AddTodo from './components/AddTodo';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+
+    export default {
+        name: "app",
+        components: {
+            Header,
+            Todos,
+            AddTodo
+        },
+        methods: {
+            deleteTodo(id) {
+                this.todos = this.todos.filter(todo => todo.id != id);
+            },
+            addTodo(newTodo){
+                this.todos = [...this.todos,newTodo];
+            }
+        },
+        data() {
+            return {
+                todos: [
+                    {
+                        id: 1,
+                        title: "Todo One",
+                        completed: false
+                    }, {
+                        id: 2,
+                        title: "Todo Two",
+                        completed: true
+                    }, {
+                        id: 3,
+                        title: "Todo Three",
+                        completed: false
+                    },
+                ]
+            }
+        }
+    };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    /* reset */
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+        line-height: 1.4;
+    }
+    .btn{
+        display: inline-block;
+        border: none;
+        background: #555;
+        color: #fff;
+        padding: 7px 20px;
+        cursor: pointer;
+    }
 </style>
